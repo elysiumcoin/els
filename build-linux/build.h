acf2bd332a02ac896200fa58c4509e7a29f61bd1
afonso <afonso@Zeus.astato>
2018-05-21 23:42:59 -0300

    Regular Update

--- build-linux/build.h
+++
@@ -1,2 +0,0 @@
-// No build information available
-#define BUILD_DATE "2018-05-15 00:13:40 -0300"
--- make-all.sh
+++ make-all.sh
@@ -35,13 +35,22 @@ cd src
 make -f makefile.unix
 cd ..
 
-cp src/elysiumd output/elysiumd
+cp src/elysiumd ../output/els/elysiumd
 rm src/elysiumd
 
-tar -czvf release/elysiumd-linux.tar.gz output/elysiumd
-tar -czvf release/wallet-elysium-qt-linux.tar.gz output/elysium-qt
-upx -9 output/elysium-qt.exe
-zip release/wallet-elysium-qt-windows.zip output/elysium-qt.exe
+tar -czvf release/elysiumd-linux.tar.gz ../output/els/elysiumd
+tar -czvf release/wallet-elysium-qt-linux.tar.gz ../output/els/elysium-qt
+upx -9 ../output/els/elysium-qt.exe
+zip release/wallet-elysium-qt-windows.zip ../output/els/elysium-qt.exe
+
+rm src/leveldb/libleveldb.a
+rm src/leveldb/libmemenv.a
+rm src/leveldb/port/port_posix.o
+rm src/leveldb/util/*.o
+rm src/leveldb/table/*.o
+rm src/leveldb/db/*.o
+rm src/leveldb/helpers/memenv/*.o
+
 
 
 
